f024e7525d9b14892a6d2e173ff29b7b8ab4c0a8
45609 <45609@LAPTOP-PTBJOCJA>
2021-05-26 12:20:44 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class Product
+    {
+
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Product(string id)
+        {
+            this.Id = id;
+            switch (id)
+            {
+                case "A":
+                    this.Price = 50m;
+
+                    break;
+                case "B":
+                    this.Price = 30m;
+
+                    break;
+                case "C":
+                    this.Price = 20m;
+
+                    break;
+                case "D":
+                    this.Price = 2015m;
+                    break;
+            }
+        }
+
+    }
+}
